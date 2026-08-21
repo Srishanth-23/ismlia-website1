f8ce22c5addf9c3e431d58263904ec035cfa4d78
Nimalan Mani <mnimalanmani@gmail.com>
2026-08-21 13:04:00 +0530

    Update Google Apps Script URL in Register.jsx to the new endpoint

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -114,7 +114,7 @@ export default function Register() {
       }
 
       // Live Google Apps Script Web App URL
-      const scriptUrl = 'https://script.google.com/macros/s/AKfycbwaVZRHkQOL_RKjHxmIdOglNM7veFwLdNUpPm4iCFQt3KNXOpW7a3qsQesT-v62MzRZ/exec'
+      const scriptUrl = 'https://script.google.com/macros/s/AKfycbxfZq12z-YCvTOeEOT2I7q-Ea2kJ0z6C36e1DrMbTxYhisc4K69vNTeW6G-VYLoR5rb/exec'
       
       let driveFolderUrl = 'https://drive.google.com/drive/folders/1b-T9_6l7E-vE_X-ismlia26-posters-placeholder'
 
